ebcb70eac49a3bf03229f0b16c363e2108e19f60
Muhammad Rehan <meherrehan591@gmail.com>
2025-06-13 14:49:41 +0500

    Fix: Ignore ESLint warnings during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ⬅️ یہ لائن add کریں
+  },
 };
 
 export default nextConfig;
